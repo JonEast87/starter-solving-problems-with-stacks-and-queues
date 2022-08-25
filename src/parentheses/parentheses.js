d197870dd6466aa5e9017355f45cebb08a8ab86d
Jon Eastin <picardncce@gmail.com>
2022-08-25 09:33:52 -0700

    parantheses done

--- src/parentheses/parentheses.js
+++ src/parentheses/parentheses.js
@@ -1,5 +1,42 @@
-const Stack = require("../lib/stack");
+const Stack = require('../lib/stack')
 
-const match = (expression) => {};
+const match = (expression) => {
+	/*
+    initialize an empty stack
+    iterate through characters
+      if character is === `(:`
+        push onto the stack
+      else
+        if character is === `):`
+          if the stack isn't empty
+            pop one item off the stack
+          else
+            return false
+    if stack is empty
+      return true
+    else
+      return false
+  */
 
-module.exports = match;
+	const expStack = new Stack()
+	for (let i = 0; i < expression.length; i++) {
+		if (expression[i] === '(') {
+			expStack.push(expression[i])
+		} else {
+			if (expression[i] === ')') {
+				if (expStack.top) {
+					expStack.pop()
+				} else {
+					return false
+				}
+			}
+		}
+	}
+	if (!expStack.top) {
+		return true
+	} else {
+		return false
+	}
+}
+
+module.exports = match
